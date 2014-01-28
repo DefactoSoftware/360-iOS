10044437236c548efb40148170163984df76de2f
Girgis Ghattas <girgis@ghattas.nl>
2014-01-28 16:58:54 +0100

    Add singleton APIClient with base URL

---
+++ Three Sixty Feedback/Networking/TSFAPIClient.m
@@ -0,0 +1,23 @@
+//
+//  TSFAPIClient.m
+//  Three Sixty Feedback
+//
+//  Created by Girgis on 28/01/14.
+//  Copyright (c) 2014 Defacto. All rights reserved.
+//
+
+#import "TSFAPIClient.h"
+
+@implementation TSFAPIClient
+
++ (instancetype)sharedClient {
+	static TSFAPIClient *_sharedClient = nil;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+	    _sharedClient = [[self alloc] initWithBaseURL:[NSURL URLWithString:TSFAPIBaseURL]];
+	});
+    
+	return _sharedClient;
+}
+
+@end
---
+++ Three Sixty FeedbackTests/Networking/TSFAPIClientSpec.m
@@ -0,0 +1,30 @@
+//
+//  TSFAPIClientSpec.m
+//  Three Sixty Feedback
+//
+//  Created by Girgis on 28/01/14.
+//  Copyright (c) 2014 Defacto. All rights reserved.
+//
+
+#import "Kiwi.h"
+#import "TSFAPIClient.h"
+
+SPEC_BEGIN(TSFAPIClientSpec)
+
+describe(@"TSFAPIClient", ^{
+    __block TSFAPIClient *APIClient;
+    
+    beforeEach(^{
+        APIClient = [TSFAPIClient sharedClient];
+    });
+    
+    it(@"instantiates correctly", ^{
+        [[APIClient should] beKindOfClass:[TSFAPIClient class]];
+    });
+    
+    it(@"has the correct base URL", ^{
+        [[[APIClient.baseURL absoluteString] should] equal:TSFAPIBaseURL];
+    });
+});
+
+SPEC_END
