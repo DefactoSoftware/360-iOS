07f36f3cbe57cf2ad19295785341e62edee79872
jurre <jurrestender@hotmail.com>
2014-02-01 22:28:17 +0100

    make number of options explicit

--- Three Sixty Feedback/Views/Custom/TSFKeyBehaviourRatingView.m
+++ Three Sixty Feedback/Views/Custom/TSFKeyBehaviourRatingView.m
@@ -8,62 +8,76 @@
 
 #import "TSFKeyBehaviourRatingView.h"
 
-@interface TSFKeyBehaviourRatingView()
+@interface TSFKeyBehaviourRatingView ()
 @property (nonatomic, strong) UIButton *selectedButton;
 @property (nonatomic, strong) UIColor *selectedColor;
 @property (nonatomic, strong) UIColor *defaultColor;
+@property (nonatomic, assign) NSInteger numberOfOptions;
 @end
 
 @implementation TSFKeyBehaviourRatingView
 
+- (id)initWithCoder:(NSCoder *)aDecoder {
+	self = [super initWithCoder:aDecoder];
+	if (self) {
+		[self sharedSetup];
+	}
+	return self;
+}
+
 - (id)initWithFrame:(CGRect)frame {
-    self = [super initWithFrame:frame];
-    if (self) {
-        _ratingButtons = [[NSMutableArray alloc] init];
-    }
-    return self;
+	self = [super initWithFrame:frame];
+	if (self) {
+		[self sharedSetup];
+	}
+	return self;
+}
+
+- (void)sharedSetup {
+	_ratingButtons = [[NSMutableArray alloc] init];
+	_numberOfOptions = 5;
 }
 
 - (void)layoutSubviews {
-    self.defaultColor = [UIColor blackColor];
-    self.selectedColor = [UIColor redColor];
-    
-    for (NSInteger i = 1; i <= 5; i++) {
-        UIButton *ratingButton = [self ratingButtonWithNumber:i];
-        
-        [self addSubview:ratingButton];
-        [self.ratingButtons addObject:ratingButton];
-    }
+	self.defaultColor = [UIColor blackColor];
+	self.selectedColor = [UIColor redColor];
+
+	for (NSInteger i = 1; i <= _numberOfOptions; i++) {
+		UIButton *ratingButton = [self ratingButtonWithNumber:i];
+
+		[self addSubview:ratingButton];
+		[self.ratingButtons addObject:ratingButton];
+	}
 }
 
 - (UIButton *)ratingButtonWithNumber:(NSInteger)number {
-    CGFloat buttonWidth = self.frame.size.width / 10;
-    CGFloat buttonHeight = buttonWidth;
-    CGFloat buttonX = ((number * 2) - 2) * buttonWidth + (buttonWidth / 2);
-    CGFloat buttonY = 0;
-    
-    CGRect buttonRect = CGRectMake(buttonX, buttonY, buttonWidth, buttonHeight);
-    UIButton *button = [[UIButton alloc] initWithFrame:buttonRect];
-    [button setTitle:[NSString stringWithFormat:@"%d", number] forState:UIControlStateNormal];
-    button.backgroundColor = self.defaultColor;
-    button.layer.cornerRadius = buttonWidth / 2;
+	CGFloat buttonWidth = self.frame.size.width / (_numberOfOptions * 2);
+	CGFloat buttonHeight = buttonWidth;
+	CGFloat buttonX = ((number * 2) - 2) * buttonWidth + (buttonWidth / 2);
+	CGFloat buttonY = 0;
 
-    button.titleLabel.font = [UIFont fontWithName:@"Diwan Mishafi" size:13.0f];
-    button.titleLabel.textAlignment = NSTextAlignmentCenter;
-    
-    button.tag = number;
-    
-    [button addTarget:self action:@selector(changeSelectedButton:) forControlEvents:UIControlEventTouchDown];
-    
-    return button;
+	CGRect buttonRect = CGRectMake(buttonX, buttonY, buttonWidth, buttonHeight);
+	UIButton *button = [[UIButton alloc] initWithFrame:buttonRect];
+	[button setTitle:[NSString stringWithFormat:@"%ld", number] forState:UIControlStateNormal];
+	button.backgroundColor = self.defaultColor;
+	button.layer.cornerRadius = buttonWidth / 2;
+
+	button.titleLabel.font = [UIFont fontWithName:@"Diwan Mishafi" size:13.0f];
+	button.titleLabel.textAlignment = NSTextAlignmentCenter;
+
+	button.tag = number;
+
+	[button addTarget:self action:@selector(changeSelectedButton:) forControlEvents:UIControlEventTouchDown];
+
+	return button;
 }
 
 - (void)changeSelectedButton:(UIButton *)pressedButton {
-    self.selectedButton.backgroundColor = self.defaultColor;
-    pressedButton.backgroundColor = self.selectedColor;
-    
-    self.selectedButton = pressedButton;
-    self.selectedRating = pressedButton.tag;
+	self.selectedButton.backgroundColor = self.defaultColor;
+	pressedButton.backgroundColor = self.selectedColor;
+
+	self.selectedButton = pressedButton;
+	self.selectedRating = pressedButton.tag;
 }
 
 @end
