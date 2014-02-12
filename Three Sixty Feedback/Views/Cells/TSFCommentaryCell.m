6a68725ec5378c795f69031d4da95cbf699802bb
Girgis Ghattas <girgis@ghattas.nl>
2014-02-12 20:11:47 +0100

    add commentary tableview cell in storyboards

---
+++ Three Sixty Feedback/Views/Cells/TSFCommentaryCell.m
@@ -0,0 +1,13 @@
+//
+//  TSFCommentaryCell.m
+//  Three Sixty Feedback
+//
+//  Created by Girgis Ghattas on 12-02-14.
+//  Copyright (c) 2014 Defacto. All rights reserved.
+//
+
+#import "TSFCommentaryCell.h"
+
+@implementation TSFCommentaryCell
+
+@end
