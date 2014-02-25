42fb368159ad486dbdb987c41205e1a11862c0e5
Girgis Ghattas <girgis@ghattas.nl>
2014-02-25 15:08:31 +0100

    Add basic UserQuestionnaireAssessorsViewController with tableview:

---
+++ Three Sixty Feedback/Controllers/TSFUserQuestionnaireAssessorsViewController.m
@@ -0,0 +1,13 @@
+//
+//  TSFUserQuestionnaireAssessorsViewController.m
+//  Three Sixty Feedback
+//
+//  Created by Girgis Ghattas on 25-02-14.
+//  Copyright (c) 2014 Defacto. All rights reserved.
+//
+
+#import "TSFUserQuestionnaireAssessorsViewController.h"
+
+@implementation TSFUserQuestionnaireAssessorsViewController
+
+@end
