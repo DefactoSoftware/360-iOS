60f6694c98e90f7a13feade89f9e17f68162bcd8
Girgis Ghattas <girgis@ghattas.nl>
2014-02-25 15:01:11 +0100

    Display basic questionnaire info

--- Three Sixty Feedback/Controllers/TSFUserQuestionnaireInfoViewController.m
+++ Three Sixty Feedback/Controllers/TSFUserQuestionnaireInfoViewController.m
@@ -7,7 +7,45 @@
 //
 
 #import "TSFUserQuestionnaireInfoViewController.h"
+#import "TSFUserQuestionnaireViewController.h"
+#import "TSFGenerics.h"
+
+@interface TSFUserQuestionnaireInfoViewController()
+@property (nonatomic, strong) TSFUserQuestionnaireViewController *userQuestionnaireViewController;
+@end
 
 @implementation TSFUserQuestionnaireInfoViewController
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self sharedSetup];
+    }
+    return self;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        [self sharedSetup];
+    }
+    return self;
+}
+
+- (void)sharedSetup {
+    _userQuestionnaireViewController = (TSFUserQuestionnaireViewController *)self.tabBarController;
+}
+
+- (void)viewDidLoad {
+    TSFQuestionnaire *questionnaire = self.userQuestionnaireViewController.questionnaire;
+    
+    self.tabBarController.tabBarItem.title = TSFLocalizedString(@"TSFUserQuestionnaireInfoViewControllerTab", @"My questionnaire");
+    
+    NSString *subjectFormat = TSFLocalizedString(@"TSFUserQuestionnaireInfoViewControllerSubjectFormat", @"This questionnaire is about %@.");
+    NSString *templateFormat = TSFLocalizedString(@"TSFUserQuestionnaireInfoViewControllerTemplateFormat", @"(A %@ questionnaire)");
+    self.subjectLabel.text = [NSString stringWithFormat:subjectFormat, questionnaire.subject];
+    self.titleLabel.text = [NSString stringWithFormat:templateFormat, questionnaire.title];
+    self.descriptionLabel.text = questionnaire.questionnaireDescription;
+}
 
 @end
