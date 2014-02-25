0a2c8686b1cd0d2aa4551610b24cd4113381cabd
Girgis Ghattas <girgis@ghattas.nl>
2014-02-25 12:15:12 +0100

    Add empty UsersQuestionnaireViewController

---
+++ Three Sixty Feedback/Controllers/TSFUserQuestionnaireViewController.h
@@ -0,0 +1,14 @@
+//
+//  TSFUserQuestionnaireViewController.h
+//  Three Sixty Feedback
+//
+//  Created by Girgis Ghattas on 25-02-14.
+//  Copyright (c) 2014 Defacto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSFUserQuestionnaireViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITableView *assessorsTableView;
+
+@end
---
+++ Three Sixty Feedback/Controllers/TSFUserQuestionnaireViewController.m
@@ -0,0 +1,13 @@
+//
+//  TSFUserQuestionnaireViewController.m
+//  Three Sixty Feedback
+//
+//  Created by Girgis Ghattas on 25-02-14.
+//  Copyright (c) 2014 Defacto. All rights reserved.
+//
+
+#import "TSFUserQuestionnaireViewController.h"
+
+@implementation TSFUserQuestionnaireViewController
+
+@end
